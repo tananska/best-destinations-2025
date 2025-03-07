b0a5b3652c3cd67142bf7350c8f0989cdd1cf398
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-07 18:47:59 +0200

    Add home page
    Install latest tailwind

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,68 +1,38 @@
-import { Link } from 'react-router'
-import Header from './Header'
-
+import homeView from '../assets/images/homeView.jpg'
 export default function Home() {
     return (
-        <>
-            <Header />
-            <div className="relative isolate px-6 pt-14 lg:px-8">
-                <div
-                    aria-hidden="true"
-                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-                >
-                    <div
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                        className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                    />
-                </div>
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                            Announcing our next round of funding.{' '}
-                            <Link to="#" className="font-semibold text-indigo-600">
-                                <span aria-hidden="true" className="absolute inset-0" />
-                                Read more <span aria-hidden="true">&rarr;</span>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="text-center">
-                        <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-                            Data to enrich your online business
-                        </h1>
-                        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                            fugiat veniam occaecat.
-                        </p>
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <Link
-                                to="#"
-                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Get started
-                            </Link>
-                            <Link to="#" className="text-sm/6 font-semibold text-gray-900">
-                                Learn more <span aria-hidden="true">→</span>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    aria-hidden="true"
-                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                >
-                    <div
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                        className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                    />
-                </div>
+
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            >
             </div>
 
-        </>
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+
+                <div className="sm:mb-8 sm:flex sm:justify-center">
+                    <div className="flex items-center gap-2 p-2 border rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20 max-w-md w-full">
+                        <input
+                            type="text"
+                            placeholder="Country name..."
+                            className="px-3 py-1 text-sm text-gray-600 outline-none bg-transparent flex-1"
+                        />
+                        <button className="px-4 py-1 text-sm text-white bg-indigo-600 rounded-full hover:bg-indigo-700">
+                            Search
+                        </button>
+                    </div>
+                </div>
+                <div className="flex justify-center items-center w-[1000px] h-[500px] sm:h-[700px] md:h-[800px] lg:h-[600px]">
+                    <div className="relative w-full sm:w-[130%] lg:w-[140%] h-full">
+                        <img
+                            src={homeView}
+                            alt="background"
+                            className="object-cover w-full h-full transform translate-x-[-15%]"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
