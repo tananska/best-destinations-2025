86cc3e17a3c4cb9cb1f4c671588a6cc88ff23be4
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-07 18:49:01 +0200

    Add components to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import { Route, Routes } from 'react-router'
 import Home from './components/Home'
-import './App.css'
+import Header from './components/Header'
+import './assets/App.css'
+import Footer from './components/Footer'
+import About from './components/About'
+import Catalog from './components/Catalog'
 
 function App() {
   return (
     <>
-      <div className="bg-white">
+      <Header />
+      <div id='main'>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
+      <Footer />
     </>
   )
 }
