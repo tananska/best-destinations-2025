c68f57dcab13f567ac1d0daee0bba6a30b42f950
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-08 20:09:23 +0200

    Change some functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,73 +12,69 @@ const navigation = [
     { name: 'Login', href: '/login' },
 ]
 
-
-export default function Header(){
+export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
         <header className="absolute inset-x-0 top-0 z-50">
-        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-            <div className="flex lg:flex-1">
-                <Link to="/" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Back to home</span>
-                    <img
-                        alt=""
-                        src="https://t4.ftcdn.net/jpg/00/35/85/49/360_F_35854996_U93wcfpw8bIJrO1ftyC92aLEh0AlOMSj.jpg"
-                        className="h-20 w-20"
-                    />
-                </Link>
-            </div>
-            <div className="flex lg:hidden">
-                <button
-                    type="button"
-                    onClick={() => setMobileMenuOpen(true)}
-                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                >
-                    <span className="sr-only">Open main menu</span>
-                    <Bars3Icon aria-hidden="true" className="size-6" />
-                </button>
-            </div>
-            <div className="hidden lg:flex lg:gap-x-12">
-                {navigation.map((item) => (
-                    <Link key={item.name} to={item.href} className="text-sm/6 font-semibold text-gray-900">
-                        {item.name}
+            <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <Link to="/" className="-m-1.5 p-1.5">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 translate-x-20">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
                     </Link>
-                ))}
-            </div>
-            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            </div>
-        </nav>
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-            <div className="fixed inset-0 z-50" />
-            <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                <div className="flex items-center justify-between">
+                </div>
+                <div className="flex lg:hidden">
                     <button
                         type="button"
-                        onClick={() => setMobileMenuOpen(false)}
-                        className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                     >
-                        <span className="sr-only">Close menu</span>
-                        <XMarkIcon aria-hidden="true" className="size-6" />
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="size-6" />
                     </button>
                 </div>
-                <div className="mt-6 flow-root">
-                    <div className="-my-6 divide-y divide-gray-500/10">
-                        <div className="space-y-2 py-6">
-                            {navigation.map((item) => (
-                                <Link
-                                    key={item.name}
-                                    to={item.href}
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                                >
-                                    {item.name}
-                                </Link>
-                            ))}
+                <div className="hidden lg:flex lg:gap-x-12">
+                    {navigation.map((item) => (
+                        <Link key={item.name} to={item.href} className="text-sm/6 font-semibold text-gray-900">
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                </div>
+            </nav>
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-50" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(false)}
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon aria-hidden="true" className="size-6" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <Link
+                                        key={item.name}
+                                        to={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                    >
+                                        {item.name}
+                                    </Link>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </DialogPanel>
-        </Dialog>
-    </header>
+                </DialogPanel>
+            </Dialog>
+        </header>
     )
 }
