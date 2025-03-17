da7bb73c6d666db46496d717a096f413c58ba268
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-17 19:03:10 +0200

    Add api folder

---
+++ client/src/api/authApi.js
@@ -0,0 +1,12 @@
+export const useLogin = () => {
+
+    const baseUrl = 'http://localhost:3030/users';
+    
+    const login = (username,password) => {
+        
+    }
+
+    return {
+        login,
+    }
+}
