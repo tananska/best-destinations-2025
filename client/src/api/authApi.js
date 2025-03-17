6d048f476742b0e816c77cb91ece0c664c29d64c
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-17 19:15:43 +0200

    Add abort controller to login hook

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
+import request from "../utils/request";
+
+const baseUrl = 'http://localhost:3030/users';
+
 export const useLogin = () => {
 
-    const baseUrl = 'http://localhost:3030/users';
-    
-    const login = (username,password) => {
-        
+    const abortController = new AbortController();
+
+    const login = async (email, password) => {
+        const result = await request.post(`${baseUrl}/login`, { email, password }, { signal: abortController.signal });
+
+        return result;
     }
+
+    useEffect(() => {
+        return () => abortController.abort();
+    });
 
     return {
         login,
