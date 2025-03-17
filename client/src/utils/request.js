3633c68191b074dd6b9b707bf69c88cc9eb9251c
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-17 19:10:19 +0200

    Add request util

---
+++ client/src/utils/request.js
@@ -0,0 +1,28 @@
+const request = async (method, url, data, options = {}) => {
+
+    if (method !== 'GET') {
+        options.method = method;
+    }
+
+    if (data) {
+        options = {
+            ...options,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }
+    }
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    return result;
+};
+
+export default {
+    get: request.bind(null, 'GET'),
+    post: request.bind(null, 'POST'),
+    put: request.bind(null, 'PUT'),
+    delete: request.bind(null, 'DELETE'),
+}
