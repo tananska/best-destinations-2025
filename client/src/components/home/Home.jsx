bceb33625476647f9fe7268da5d80357e5b9c8f7
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-22 15:35:32 +0200

    Remove home picture

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,3 @@
-import Navigation from '../navigation/Navigation'
 
 import homeVideo from '../../../public/videos/homeVideo.webm'
 import './video.module.css'
@@ -8,11 +7,8 @@ export default function Home() {
             <div className="absolute inset-0">
                 <video className="w-full h-full object-cover" src={homeVideo} autoPlay loop muted />
             </div>
-            <div className="absolute top-0 left-0 w-full z-10">
-                <Navigation />
-            </div>
             <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
-                <h1 className="text-4xl font-bold">Find the best destination for you</h1>
+                <h1 className="text-4xl font-bold">Explore the world with us</h1>
             </div>
         </div>
     )
