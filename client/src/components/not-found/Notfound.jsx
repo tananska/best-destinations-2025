fda78dd12b75e7da12176a92cc0fa21f3f848d0a
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-24 16:29:38 +0200

    Change a tag to link

--- client/src/components/not-found/Notfound.jsx
+++ client/src/components/not-found/Notfound.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 export default function Notfound() {
     return (
       <>
@@ -10,12 +12,12 @@ export default function Notfound() {
               Sorry, we couldn’t find the page you’re looking for.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/"
+              <Link
+                to="/"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Go back home
-              </a>
+              </Link>
               <a href="#" className="text-sm font-semibold text-gray-900">
                 Contact support <span aria-hidden="true">&rarr;</span>
               </a>
