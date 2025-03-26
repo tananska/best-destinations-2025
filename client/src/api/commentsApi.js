e27b98784cba4106281ce7a25d328094b030aea5
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-26 13:41:11 +0200

    Add get all comments api

--- client/src/api/commentsApi.js
+++ client/src/api/commentsApi.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
 import request from "../utils/request";
 
 const baseUrl = 'http://localhost:3030/data/comments';
 
+export const useGetAllComments = (destinationId) => {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams({
+            where: `destinationId="${destinationId}"`,
+        });
+
+        request('GET', `${baseUrl}?${searchParams.toString()}`)
+            .then(setComments);
+    }, [destinationId]);
+
+    return {
+        comments
+    };
+}
+
 export const useCreateComment = () => {
     const { authorizationOptions } = useAuth();
 
-    const create = (destinationId, commentData) => request('POST', baseUrl, { destinationId, comment: commentData }, authorizationOptions);
+    const create = (destinationId, commentData, username) => request('POST', baseUrl, { destinationId, comment: commentData, username }, authorizationOptions);
 
     return {
         create
