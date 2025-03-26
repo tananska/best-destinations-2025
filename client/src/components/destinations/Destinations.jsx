9b4018dcc9a33619620cfaffdfe5390979f35aa7
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-26 15:11:13 +0200

    Check if there are any destinations

--- client/src/components/destinations/Destinations.jsx
+++ client/src/components/destinations/Destinations.jsx
@@ -11,25 +11,28 @@ export default function Destinations() {
                 <h2 className="text-2xl text-center font-bold tracking-tight text-gray-900 font-mono">Explore by click on picture</h2>
 
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {destinations.map((destination) => (
-                        <div key={destination._id} className="group relative">
-                            <img
-                                alt={destination.name}
-                                src={destination.imageUrl}
-                                className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-                            />
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-gray-700 ">
-                                        <Link to={`/destinations/${destination._id}/details`}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            {destination.name}
-                                        </Link>
-                                    </h3>
+                    {destinations.length > 0
+                        ? destinations.map((destination) => (
+                            <div key={destination._id} className="group relative">
+                                <img
+                                    alt={destination.name}
+                                    src={destination.imageUrl}
+                                    className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+                                />
+                                <div className="mt-4 flex justify-between">
+                                    <div>
+                                        <h3 className="text-sm text-gray-700 ">
+                                            <Link to={`/destinations/${destination._id}/details`}>
+                                                <span aria-hidden="true" className="absolute inset-0" />
+                                                {destination.name}
+                                            </Link>
+                                        </h3>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))
+                        : <p className="text-gray-500 dark:text-gray-400 text-center italic mt-6">No destinations yet.</p>
+                    }
                 </div>
             </div>
         </div>
