354a455d9e7041c98c6b6964494149310e638602
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-27 18:06:09 +0200

    Add error handle to register page
    Replace useActionState to controlled form

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -23,7 +23,27 @@ export const useLogin = () => {
 
 export const useRegister = () => {
 
-    const register = async (username, email, password) => {
+    const register = async (username, email, password, rePassword) => {
+
+        const usernamePattern = /^[a-z]{3,}@(abv\.bg|email\.com)$/;
+        const passwordPattern = /^(?=(.*[a-zA-Z]){5,})(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{7,}$/;
+
+        if (password !== rePassword) {
+            throw new Error('Passwords do not match. Please make sure both passwords are identical.');
+        }
+
+        if (username.length < 3) {
+            throw new Error('Username should be at least 3 characters.');
+        }
+
+        if (!usernamePattern.test(email)) {
+            throw new Error('Invalid email format! Your email must start with at least 3 lowercase letters and end with @abv.bg or @email.com.');
+        }
+
+        if (!passwordPattern.test(password)) {
+            throw new Error('Password must be at least 5 letters long, contain at least one uppercase letter, one digit, and one special character.');
+        }
+
         const response = await request('POST', `${baseUrl}/register`, { username, email, password });
 
         if (!response.accessToken) {
