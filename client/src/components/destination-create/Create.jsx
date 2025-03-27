7ae24c5dce46aa43c43ed4f18fd32bec5806c829
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-28 00:04:42 +0200

    Add background video to create page

--- client/src/components/destination-create/Create.jsx
+++ client/src/components/destination-create/Create.jsx
@@ -16,49 +16,90 @@ export default function Create() {
   }
 
   return (
-    <div className="relative h-screen bg-gray-200 dark:bg-gray-800">
-    <video 
-      autoPlay 
-      loop 
-      muted 
-      className="absolute inset-0 w-full h-full object-cover"
-    >
-      <source 
-        src={video} 
-        type="video/mp4" 
-      />
-    </video>
+    <div className="relative h-screen bg-gray-200 blue:bg-blue-500">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source
+          src={video}
+          type="video/mp4"
+        />
+      </video>
 
-    <div className="relative z-10 flex items-center justify-center h-full">
-      <div className="container bg-white p-8 shadow-lg rounded-lg">
-        <form action={formSubmit}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700">Username</label>
-            <input 
-              type="text" 
-              id="username" 
-              className="w-full px-4 py-2 border rounded" 
-              placeholder="Username" 
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">Email</label>
-            <input 
-              type="email" 
-              id="email" 
-              className="w-full px-4 py-2 border rounded" 
-              placeholder="Email" 
-            />
-          </div>
-          <button 
-            type="submit" 
-            className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-          >
-            Submit
-          </button>
-        </form>
+      <div className="relative z-10 flex items-center justify-center h-full">
+        <div className="container bg-gray-100 w-150 p-8 shadow-lg rounded-lg font-style: italic">
+          <form action={formSubmit}>
+            <div className="mb-4">
+              <label htmlFor="country" className="block text-gray-700">Country</label>
+              <input
+                type="text"
+                name="country"
+                id="country"
+                className="w-full px-4 py-2 border rounded"
+                placeholder="e.g., France"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="town" className="block text-gray-700">Town</label>
+              <input
+                type="text"
+                name="town"
+                id="town"
+                className="w-full px-4 py-2 border rounded"
+                placeholder="e.g., Paris"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="w-full px-4 py-2 border rounded"
+                placeholder="e.g., Eiffel Tower"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="imageUrl" className="block text-gray-700">Image url</label>
+              <input
+                type="text"
+                name="imageUrl"
+                id="imageUrl"
+                className="w-full px-4 py-2 border rounded"
+                placeholder="Url address"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">Select options</label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-4 py-2 border rounded"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="description" className="block text-gray-700">Description</label>
+              <input
+                type="text"
+                name="description"
+                id="description"
+                className="w-full px-4 py-2 border rounded"
+                placeholder="e.g., The Eiffel Tower is a wrought-iron lattice tower..."
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
